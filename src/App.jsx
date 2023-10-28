a38cd32e3fb3525711a317f520802332e6ecd99f
mrtcngnn <mrtcngonen@gmail.com>
2023-10-28 21:34:05 +0300

    player symbols can be clicked on game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+import { useState } from "react";
+import Log from "./components/Log";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+  const [gameTurns, setGameTurns] = useState([]);
+  function handleSelectSquare() {
+    setActivePlayer((currActivePlayer) =>
+      currActivePlayer === "X" ? "O" : "X"
+    );
+    setGameTurns();
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol={"X"} />
-          <Player name={"Player 2"} symbol={"O"} />
+        <ol id="players" className="highlight-player">
+          <Player
+            name="Player 1"
+            symbol={"X"}
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            name={"Player 2"}
+            symbol={"O"}
+            isActive={activePlayer === "O"}
+          />
         </ol>
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
+      <Log />
     </main>
   );
 }
