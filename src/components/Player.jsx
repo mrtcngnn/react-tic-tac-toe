76f9d1fa74b3101416399c5fbacc69c401ebed01
mrtcngnn <mrtcngonen@gmail.com>
2023-10-28 19:37:36 +0300

    player name functions

---
+++ src/components/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(name);
+
+  function handleEditClick() {
+    setIsEditing((isEditing) => !isEditing);
+  }
+
+  function handleNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let playerNameChange = <span className="player-name">{playerName}</span>;
+  if (isEditing === true) {
+    playerNameChange = (
+      <input
+        type="text"
+        defaultValue={playerName}
+        onChange={handleNameChange}
+        required
+      />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerNameChange}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
